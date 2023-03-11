8bbe4201caa6506655769f4592a99790900821ed
JayavelAnandh <jvanandh984@gmail.com>
2023-03-11 13:38:31 +0530

    Added vercelJson

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,7 +15,7 @@ const isAuthorized = async(req,res,next)=>{
             next()
         } catch (error) {
             console.log(error)
-            res.status(401).json({message:"Invalid Authorization"})
+            res.status(401).send(error)
         }
     }
 
