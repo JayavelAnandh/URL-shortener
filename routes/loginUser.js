7876204b04b7f6f343deec448950c17f765d2b2c
JayavelAnandh <jvanandh984@gmail.com>
2023-03-11 15:42:25 +0530

    Trying to clear some Errors

--- routes/loginUser.js
+++ routes/loginUser.js
@@ -28,7 +28,7 @@ router.post("/",async(req,res)=>{
     } catch (error) {
         
         console.log(error)
-        res.status(500).send({ message: "Internal Server Error"});
+        res.status(500).send(error);
     }
     
 
